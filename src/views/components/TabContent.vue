d721d433ce2c1d8dfbcf187e6f1c09186f0d918e
thaodtp <dtpthao.its@gmail.com>
2020-11-22 15:11:35 +0700

    update light font, transferTab

--- src/views/components/TabContent.vue
+++ src/views/components/TabContent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="body"></div>
+  <div id="container">
+  </div>
 </template>
 
 <script lang="ts">
--- src/views/components/TransferTab.vue
+++ src/views/components/TransferTab.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="converter__wrap-text">
+      <h1
+        class="converter__title primary-title text-center"
+        :class="{ light__font_one: light }"
+      >
+        Fast and simple way to transfer coins for cash
+      </h1>
+      <!-- mobile title -->
+      <h1
+        class="converter__title secondary-title text-center"
+        :class="{ light__font_one: light }"
+      >
+        Transfer crypto for cash instantly
+      </h1>
+      <!-- ------------- -->
+      <p
+        class="converter__text text-center"
+        :class="{ light__font_two: light}"
+      >
+        Transfer coins under the value of $200 and receive payment
+        instantly.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,6 +35,9 @@
 
   export default class TransferTab extends Vue{
 
+    get light () {
+      return this.$store.getters.theme == 'light'
+    }
   }
 </script>
 
