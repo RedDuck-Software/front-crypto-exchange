534ecf187b913c15c3cd059f7b125f8f3a90913f
thaodtp <v.thaodtp@vincss.net>
2020-11-28 20:17:07 +0700

    update transfer components

---
+++ src/components/transferTab/ButtonsRow.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="converter__wallet-block flex flex-col flex-d-row sm:mt-4 justify-between">
+    <div class="converter__wallet-complete d-none">
+      <div class="wallet__select d-flex">
+        <div class="wallet__variant text-center" />
+      </div>
+    </div>
+
+    <ConnectWalletButton
+      :account="account"
+      @change="onAccountChange"
+      class="width50 lg:w-37%"
+    />
+
+    <c-button
+      @click="send"
+      :disabled="transferNowDisabled"
+      :class="[ifActiveLightMode, ifConnected]"
+      class="wallet__btn md:mt-0 width50 md:ml-4 lg:w-37%"
+    >
+      Transfer Now
+    </c-button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import CButton from "@/components/tags/cButton.vue";
+  import ConnectWalletButton from "@/components/transferTab/ConnectWalletButton.vue";
+
+  @Component({
+    name: 'ButtonsRow',
+    components: {ConnectWalletButton, CButton}
+  })
+
+  export default class ButtonsRow extends Vue {
+
+    public account = ''
+
+    get light () {
+      return this.$store.getters.theme == 'light'
+    }
+
+    get transferNowDisabled() {
+      return true
+    }
+
+    get ifActiveLightMode() {
+      return this.light ? "classLightMode" : "";
+    }
+
+    get ifConnected() {
+      return "classLightModeBg"
+    }
+
+    public onAccountChange() {}
+
+    public send() {}
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/components/TransferTab.vue
+++ src/views/components/TransferTab.vue
@@ -23,97 +23,11 @@
           @close="privateKeyModalVisible = false"
         />
 
-        <div class="gas__row">
-          <div class="gas__col">
-            <p
-              :class="{ light__font_two: light }"
-              class="gas__text"
-            >
-              <i class="fas fa-question-circle" />
-              <span class="gas__poppup">CACO Transaction Fee</span>
-              Service Fee =
-            </p>
+        <GasRow :service-fees="serviceFees"/>
 
-            <div class="gas__button__col">
-              <label
-                style="color: white; margin-left: 2px;"
-                :class="{ light__font_two: light }"
-              >
-                <span
-                  class="currency-value"
-                  :class="{ light__font_two: light }"
-                >
-                  {{ $store.getters.fiatType.text === 'USD' ? '$' : '€'}}
-                </span>
+        <buttons-row />
 
-                {{ serviceFees }}
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <div class="converter__wallet-block flex flex-col flex-d-row sm:mt-4 justify-between">
-          <div class="converter__wallet-complete d-none">
-            <div class="wallet__select d-flex">
-              <div class="wallet__variant text-center" />
-            </div>
-          </div>
-
-          <ConnectWalletButton
-            :account="account"
-            @change="onAccountChange"
-            class="width50 lg:w-37%"
-          />
-
-          <c-button
-            @click="send"
-            :disabled="transferNowDisabled"
-            :class="[ifActiveLightMode, ifConnected]"
-            class="wallet__btn md:mt-0 width50 md:ml-4 lg:w-37%"
-          >
-            Transfer Now
-          </c-button>
-        </div>
-
-        <div class="customInfoRow">
-          <div class="customInfoCol">
-            <div class="d-flex justify-content-center mt-2">
-              <span
-                class="agree__text"
-                :class="{ light__font_six: light}"
-              >
-                By clicking "Transfer Now" you agree to the
-                <a
-                  href="https://drive.google.com/file/d/1P8AhZuP6856r7Pq7UgMKxBjlKseuPn3O/view?usp=sharing"
-                  class="agree__link"
-                  target="_blank"
-                >
-                  Terms & Conditions
-                </a>
-              </span>
-            </div>
-
-            <label class="checkboxContainer">
-              <p
-                class="agree__text ml-1"
-                :class="{ light__font_six: light}"
-              >
-                Receive updates on CACO developments
-              </p>
-
-              <input
-                type="checkbox"
-                checked="checked"
-                :class="{ light__font_six: light }"
-              />
-
-              <span
-                class="checkboxCheckmark"
-                :class="{ checkboxCheckmark2: light }"
-              />
-            </label>
-          </div>
-        </div>
+        <custom-info-row />
 
         <video-card />
       </template>
@@ -150,10 +64,16 @@
   import CButton from "@/components/tags/cButton.vue";
   import VideoCard from "@/components/transferTab/VideoCard.vue";
   import PrivateKeyModal from "@/components/modals/PrivateKeyModal.vue";
+  import GasRow from "@/components/transferTab/GasRow.vue";
+  import CustomInfoRow from "@/components/transferTab/CustomInfoRow.vue";
+  import ButtonsRow from "@/components/transferTab/ButtonsRow.vue";
 
   @Component({
     name: 'TransferTab',
     components: {
+      ButtonsRow,
+      CustomInfoRow,
+      GasRow,
       PrivateKeyModal,
       VideoCard,
       CButton,
@@ -169,8 +89,6 @@
     public fiatAmount = 0
     public exchangeRate = 0.123
     public transferable = true
-    public serviceFees = '0.0'
-    public account = ''
     public privateKeyModalVisible = false
 
     public cryptoCurrencies: (CommonSelectBox & {
@@ -203,66 +121,16 @@
       return this.$store.getters.theme == 'light'
     }
 
-    get transferNowDisabled() {
-      return true
-    }
-
-    get ifActiveLightMode() {
-      return this.light ? "classLightMode" : "";
-    }
-
-    get ifConnected() {
-      return "classLightModeBg"
+    get serviceFees() {
+      return 0
+/*      return this.rounded(
+        MetamaskService.getFees(+this.amount * this.currencyExchangeRate)
+      );*/
     }
 
     /** ------------------------------------------------------- **/
     /** ------------------------------------------------------- **/
 
-    public onAccountChange() {}
-
-    public send() {}
   }
 
 </script>
-
-<style scoped>
-
-  .form__wrapper {
-    position: relative;
-    margin: 1.25rem auto 0;
-    width: 49.75rem;
-  }
-
-  @media screen and (min-width: 625px) {
-    .form__wrapper {
-      width: 75%;
-    }
-  }
-
-  @media screen and (min-width: 320px) and (max-width: 624px) {
-    .form__wrapper {
-      margin: 0 auto;
-      width: 100%;
-    }
-  }
-
-  @media screen and (min-width: 625px) and (max-width: 1200px) {
-    .form__wrapper {
-      width: 77%;
-    }
-  }
-
-  .customInfoRow {
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  .customInfoCol {
-    width: 37%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;
-  }
-
-</style>
