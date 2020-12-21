089506063eadd35d1899de37c0c1311ff264d29c
thaodtp <v.thaodtp@vincss.net>
2020-12-22 06:34:27 +0700

    edit CustomInfoRow

--- src/components/transferTab/CustomInfoRow.vue
+++ src/components/transferTab/CustomInfoRow.vue
@@ -1,39 +1,39 @@
 <template>
-  <div class="customInfoRow d-flex justify-content-center mt-2">
-    <p
-      class="agree__text w-full"
-      :class="{ light__font_six: light}"
-    >
-      By clicking "Transfer Now" you agree to the
-      <a
-        href="https://drive.google.com/file/d/1P8AhZuP6856r7Pq7UgMKxBjlKseuPn3O/view?usp=sharing"
-        class="agree__link"
-        target="_blank"
-      >
-        Terms & Conditions
-      </a>
-    </p>
-
-    <label class="checkboxContainer flex mx-auto order-3">
-      <p
-        class="agree__text ml-1 order-2"
-        :class="{ light__font_six: light}"
-      >
-        Receive updates on CACO developments
-      </p>
-
-      <input
-        type="checkbox"
-        checked="checked"
-        :class="{ light__font_six: light }"
-        class="checkboxContainer__check order-1"
-      />
-
-      <span
-        class="checkboxCheckmark"
-        :class="{ checkboxCheckmark2: light }"
-      />
-    </label>
+  <div class="customInfoRow">
+    <div class="customInfoCol">
+      <div class="d-flex justify-content-center mt-2">
+        <span
+          class="agree__text"
+          :class="{ light__font_six: light }"
+        >
+          By clicking "Transfer Now" you agree to the
+          <a
+            href="https://drive.google.com/file/d/1P8AhZuP6856r7Pq7UgMKxBjlKseuPn3O/view?usp=sharing"
+            class="agree__link"
+            target="_blank"
+          >
+            Terms & Conditions
+          </a>
+        </span>
+      </div>
+      <label class="checkboxContainer">
+        <p
+          class="agree__text ml-1"
+          :class="{ light__font_six: light }"
+        >
+          Receive updates on CACO developments
+        </p>
+        <input
+          type="checkbox"
+          checked="checked"
+          :class="{ light__font_six: light }"
+        />
+        <span
+          class="checkboxCheckmark"
+          :class="{ checkboxCheckmark2: light }"
+        ></span>
+      </label>
+    </div>
   </div>
 </template>
 
